82da806491a17a82e2fb28dc8590a8aca16c668f
raj3k <rajkosky@gmail.com>
2023-10-11 21:57:13 +0200

    test: update test cases in counter_test.

--- internal/counter/counter_test.go
+++ internal/counter/counter_test.go
@@ -3,6 +3,7 @@ package counter
 import "testing"
 
 func TestMostFrequentWords(t *testing.T) {
+	// given
 	tests := []struct {
 		input    string
 		num      int
@@ -13,23 +14,25 @@ func TestMostFrequentWords(t *testing.T) {
 			num:   3,
 			expected: []WordCount{
 				{"sample", 3},
-				{"This", 2},
-				{"is", 2},
+				{"this", 3},
+				{"a", 2},
 			},
 		},
 		{
 			input: "Hello world! Hello Go! Go is awesome!",
 			num:   2,
 			expected: []WordCount{
-				{"Hello", 2},
-				{"Go", 2},
+				{"go", 2},
+				{"hello", 2},
 			},
 		},
 	}
 
 	for _, test := range tests {
+		// when
 		result := MostFrequentWords(test.input, test.num)
 
+		// then
 		if len(result) != len(test.expected) {
 			t.Errorf("Expected %d most frequent words, but got %d", len(test.expected), len(result))
 		}
